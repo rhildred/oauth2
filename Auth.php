2292ed565f56cae2687c261f740ab947b1832f1d
Richard Hildred <rhildred@gmail.com>
2015-11-30 22:15:54 -0500

    updated to allow the url if the session CurrentUser is set

--- Auth.php
+++ Auth.php
@@ -3,7 +3,10 @@
 class Auth
 {
     public function __invoke($route){
-        $app = \Slim\Slim::getInstance();
-        $app->halt(500, "error ... login required");
+        session_start();
+        if(!array_key_exists("CurrentUser", $_SESSION)){
+            $app = \Slim\Slim::getInstance();
+            $app->halt(500, "error ... login required");
+        }
     }
 }
